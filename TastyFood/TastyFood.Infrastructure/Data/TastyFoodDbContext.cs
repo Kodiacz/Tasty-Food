8c53b6695f7903093b02cbb310855cd18a773ce6
Simeon Yordanov <lookmanche@gmail.com>
2022-11-07 15:43:54 +0200

    Created DbSets of all the data models

--- TastyFood/TastyFood.Infrastructure/Data/TastyFoodDbContext.cs
+++ TastyFood/TastyFood.Infrastructure/Data/TastyFoodDbContext.cs
@@ -2,6 +2,7 @@
 {
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
+    using TastyFood.Infrastructure.Data.Entities;
 
     public class TastyFoodDbContext : IdentityDbContext
     {
@@ -9,5 +10,28 @@
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Detail> Details { get; set; }
+
+        public DbSet<Direction> Directions { get; set; }
+
+        public DbSet<Ingredient> Ingredients { get; set; }
+
+        public DbSet<NutritionFact> NutritionFacts { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Recipe> Recipes { get; set; }
+
+        public DbSet<ShoppingList> ShoppingLists { get; set; }
+
+        public DbSet<Step> Steps { get; set; }
     }
 }
