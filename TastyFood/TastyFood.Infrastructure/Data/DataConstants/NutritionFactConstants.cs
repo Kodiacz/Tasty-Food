f55bd5e1939441f055baa8ec2e1916cefa930235
Simeon Yordanov <lookmanche@gmail.com>
2022-11-21 13:46:32 +0200

    deleted NutritionFactConstants.cs

--- TastyFood/TastyFood.Infrastructure/Data/DataConstants/NutritionFactConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TastyFood.Infrastructure.Data.DataConstants
-{
-    public static class NutritionFactConstants
-    {
-        public const int NameMaxLength = 20;
-        public const int NameMinLength = 2;
-    }
-}
