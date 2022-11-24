eede50ea95255c752a6ce37e2cc8bccbf906410b
Simeon Yordanov <lookmanche@gmail.com>
2022-11-24 22:23:44 +0200

    added InverseProperty
    OwnRecipes and FavoriteRecpes in ApplicationUser now they have their InverseProperties

--- TastyFood/TastyFood.Infrastructure/Data/Entities/ApplicationUser.cs
+++ TastyFood/TastyFood.Infrastructure/Data/Entities/ApplicationUser.cs
@@ -2,6 +2,7 @@
 {
     using Microsoft.AspNetCore.Identity;
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
     using static TastyFood.Infrastructure.Data.DataConstants.ApplicationUserConstants;
 
     public class ApplicationUser : IdentityUser
@@ -23,8 +24,10 @@
 
         public virtual ICollection<ShoppingList> ShoppingLists { get; set; }
 
+        [InverseProperty(nameof(Recipe.UserOwner))]
         public virtual ICollection<Recipe> OwnRecipes { get; set; }
 
+        [InverseProperty(nameof(Recipe.UsersFavoriteRecipes))]
         public virtual ICollection<Recipe> FavoriteRecipes { get; set; }
     }
 }
