e00ad9595ebd295dda23133420b2eb3ab6e1a5d3
Simeon Yordanov <lookmanche@gmail.com>
2022-12-08 16:04:03 +0200

    minor changes

---
+++ TastyFood/TastyFood.Infrastructure/Data/DataConstants/ErrorConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace TastyFood.Infrastructure.Data.DataConstants
+{
+    public class ErrorConstants
+    {
+        public const string ProductMessage = "ProductMessage";
+        public const string QuantityMessage = "QuantityMessage";
+        public const string StepMessage = "StepMessage";
+    }
+}
