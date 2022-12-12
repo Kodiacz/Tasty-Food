b97e530915e1f2813928dbb29925543d1d0bd520
Simeon Yordanov <lookmanche@gmail.com>
2022-12-12 13:32:25 +0200

    changed the collection type to List
    the change is made due to need of the Add() method

--- TastyFood/TastyFood.Core/Models/RecipeModels/CreateRecipeViewModel.cs
+++ TastyFood/TastyFood.Core/Models/RecipeModels/CreateRecipeViewModel.cs
@@ -39,8 +39,8 @@
         [Range(ServingsQuantityMinLength, ServingsQuantityMaxLength)]
         public int ServingsQuantity { get; set; }
 
-        public ICollection<IngredientViewModel> Ingredients { get; set; }
+        public List<IngredientViewModel> Ingredients { get; set; }
 
-        public ICollection<DirectionViewModel> Directions { get; set; }
+        public List<DirectionViewModel> Directions { get; set; }
     }
 }
