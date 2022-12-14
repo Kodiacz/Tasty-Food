24a68cf768de4447e00e1145eb4e3addddcd6112
Simeon Yordanov <lookmanche@gmail.com>
2022-12-14 17:34:20 +0200

    added summery comments

--- TastyFood/TastyFood.Core/Contracts/IShoppingListService.cs
+++ TastyFood/TastyFood.Core/Contracts/IShoppingListService.cs
@@ -6,12 +6,37 @@ namespace TastyFood.Core.Contracts
 {
     public interface IShoppingListService
     {
+        /// <summary>
+        /// Creates a ShoppingListViewModel and asigns its properties with the DetailRecipeViewModel 
+        /// that is passed as a parametar
+        /// </summary>
+        /// <param name="model">parameter object of type DetailRecipeViewModel</param>
+        /// <param name="currentUserId">parameter of type string which contains the current user id</param>
+        /// <param name="currentUserName">parameter of type string which contains the current user username</param>
+        /// <returns>returns object of type CreateShoppingListViewModel</returns>
         public CreateShoppingListViewModel CreateShoppingListViewModel(DetailRecipeViewModel model, string currentUserId, string currentUserName);
 
+        /// <summary>
+        /// Creates a ShoppingList entity and assigns its properties from the 
+        /// model of type CreateShoppingListViewModel that is passed as a parameter
+        /// </summary>
+        /// <param name="model">parameter object of type CreateShoppingListViewModel</param>
+        /// <param name="recipeId">parameter of type int that contains the id of a Recipe entity</param>
+        /// <returns></returns>
         public Task CreateShoppintListAsync(CreateShoppingListViewModel model, int recipeId);
 
+        /// <summary>
+        /// Gets the owner of the recipe 
+        /// </summary>
+        /// <param name="recipeId">parameter of type int that contains the id of the Recipe entity</param>
+        /// <returns>returns an object of type ApplicationUser</returns>
         public ApplicationUser GetOwnerOfRecipe(int recipeId);
 
+        /// <summary>
+        /// Gets the id of the current user ShoppingList
+        /// </summary>
+        /// <param name="currentUser">parameter of type string that contains the id of the current user</param>
+        /// <returns>returns an integer that contains the id of the current user ShoppingList</returns>
         public int? GetCurrentUserShoppingListId(string currentUser);
     }
 }
--- TastyFood/TastyFood.Core/Services/ShoppingListService.cs
+++ TastyFood/TastyFood.Core/Services/ShoppingListService.cs
@@ -17,6 +17,14 @@
             this.repo = repo;
         }
 
+        /// <summary>
+        /// Creates a ShoppingListViewModel and asigns its properties with the DetailRecipeViewModel 
+        /// that is passed as a parametar
+        /// </summary>
+        /// <param name="model">parameter object of type DetailRecipeViewModel</param>
+        /// <param name="currentUserId">parameter of type string which contains the current user id</param>
+        /// <param name="currentUserName">parameter of type string which contains the current user username</param>
+        /// <returns>returns object of type CreateShoppingListViewModel</returns>
         public CreateShoppingListViewModel CreateShoppingListViewModel(DetailRecipeViewModel model, string currentUserId, string currentUserName)
         {
             return new CreateShoppingListViewModel
@@ -32,6 +40,13 @@
             };
         }
 
+        /// <summary>
+        /// Creates a ShoppingList entity and assigns its properties from the 
+        /// model of type CreateShoppingListViewModel that is passed as a parameter
+        /// </summary>
+        /// <param name="model">parameter object of type CreateShoppingListViewModel</param>
+        /// <param name="recipeId">parameter of type int that contains the id of a Recipe entity</param>
+        /// <returns></returns>
         public async Task CreateShoppintListAsync(CreateShoppingListViewModel model, int recipeId)
         {
             ShoppingList shoppingEntity = new ShoppingList()
@@ -51,6 +66,11 @@
             await this.repo.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Gets the owner of the recipe 
+        /// </summary>
+        /// <param name="recipeId">parameter of type int that contains the id of the Recipe entity</param>
+        /// <returns>returns an object of type ApplicationUser</returns>
         public ApplicationUser GetOwnerOfRecipe(int recipeId)
         {
             ApplicationUser recipeOwner = this.repo.AllReadonly<ApplicationUser>()
@@ -60,6 +80,11 @@
             return recipeOwner;
         }
 
+        /// <summary>
+        /// Gets the id of the current user ShoppingList
+        /// </summary>
+        /// <param name="currentUser">parameter of type string that contains the id of the current user</param>
+        /// <returns>returns an integer that contains the id of the current user ShoppingList</returns>
         public int? GetCurrentUserShoppingListId(string currentUser)
         {
             return this.repo
