99c8d21f582c9b6601091272593b617f1b6970a7
Simeon Yordanov <lookmanche@gmail.com>
2022-12-14 10:19:01 +0200

    moved all js function to js file to cleaner cshtml files

---
+++ TastyFood/TastyFood/wwwroot/js/DetailJavascript.js
@@ -0,0 +1,36 @@
+﻿let checkBoxes = Array.from(document.querySelectorAll('.ingredients'))
+
+let downloadBtn = document.querySelector('#download-btn')
+let saveBtn = document.querySelector('#save-btn')
+
+checkBoxes.forEach(cb => cb.addEventListener('change', checkButtonStatus))
+
+function checkButtonStatus() {
+    let checkedCount = checkBoxes.filter(cb => cb.checked)
+
+    if (checkedCount.length == 0) {
+        downloadBtn.disabled = true
+        saveBtn.disabled = true
+    } else {
+        downloadBtn.disabled = false
+        saveBtn.disabled = false
+    }
+}
+
+function CheckAllIngredients() {
+    let checkedCount = checkBoxes.filter(cb => cb.checked)
+
+    if (checkedCount < checkBoxes.length) {
+        for (var i = 0; i < checkBoxes.length; i++) {
+            checkBoxes[i].checked = true
+        }
+    } else {
+        for (var i = 0; i < checkBoxes.length; i++) {
+            checkBoxes[i].checked = false
+        }
+    }
+
+    checkButtonStatus()
+}
+
+checkButtonStatus()
---
+++ TastyFood/TastyFood/wwwroot/js/popoverJs.js
@@ -0,0 +1,4 @@
+﻿var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTrigger) {
+    return new bootstrap.Popover(popoverTrigger)
+})
