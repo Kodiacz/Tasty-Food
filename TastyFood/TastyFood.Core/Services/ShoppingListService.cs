23abac59178fb318d1215f676c8230f961a140f8
Simeon Yordanov <lookmanche@gmail.com>
2022-12-14 10:06:11 +0200

    implemented the new methods for the ShoppingService

--- TastyFood/TastyFood.Core/Services/ShoppingListService.cs
+++ TastyFood/TastyFood.Core/Services/ShoppingListService.cs
@@ -1,12 +1,54 @@
 ﻿namespace TastyFood.Core.Services
 {
+    using System.Collections.Generic;
     using TastyFood.Core.Contracts;
+    using TastyFood.Core.Models.IngredientModels;
+    using TastyFood.Core.Models.RecipeModels;
+    using TastyFood.Core.Models.ShoppingListModels;
+    using TastyFood.Infrastructure.Data.Common;
+    using TastyFood.Infrastructure.Data.Entities;
 
     public class ShoppingListService : IShoppingListService
     {
-        public Task CreateShoppintList()
+        private readonly IRepository repo;
+
+        public ShoppingListService(IRepository repo)
         {
-            throw new NotImplementedException();
+            this.repo = repo;
+        }
+
+        public CreateShoppingListViewModel CreateShoppingListViewModel(DetailRecipeViewModel model, string currentUserId, string currentUserName)
+        {
+            return new CreateShoppingListViewModel
+            {
+                Name = model.Title,
+                Ingredients = model.IngredientsOutput.Select(i => new IngredientViewModel
+                {
+                    Product = i.Split(" - ")[0],
+                    Quantity = i.Split(" - ")[1],
+                }).ToList(),
+                Username = currentUserName,
+                UserId = currentUserId
+            };
+        }
+
+        public async Task CreateShoppintListAsync(CreateShoppingListViewModel model, int recipeId)
+        {
+            ShoppingList shoppingEntity = new ShoppingList()
+            {
+                Name = model.Name,
+                UserId = model.UserId,
+            };
+
+            var ingredientsEntities = this.repo.All<Ingredient>().Where(i => i.RecipeId == recipeId).ToHashSet();
+
+            foreach (var recipe in ingredientsEntities)
+            {
+                shoppingEntity.Ingredients.Add(recipe);
+            }
+
+            await this.repo.AddAsync(shoppingEntity);
+            await this.repo.SaveChangesAsync();
         }
     }
 }
