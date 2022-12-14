fc3537d163e540b55308a898d49bab94f6bfc3f9
Simeon Yordanov <lookmanche@gmail.com>
2022-12-14 17:15:03 +0200

    added new property for ShoppingList id

--- TastyFood/TastyFood.Core/Models/RecipeModels/DetailRecipeViewModel.cs
+++ TastyFood/TastyFood.Core/Models/RecipeModels/DetailRecipeViewModel.cs
@@ -16,6 +16,8 @@
 
         public int Id { get; set; }
 
+        public int? ShoppingListId { get; set; }
+
         public string Title { get; set; } = null!;
 
         public string Description { get; set; } = null!;
