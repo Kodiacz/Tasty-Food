e47503f2419667d95cf86c25298931ce3da6e83e
Simeon Yordanov <lookmanche@gmail.com>
2022-12-14 10:10:31 +0200

    changed the name of the view model

---
+++ TastyFood/TastyFood.Core/Models/ShoppingListModels/CreateShoppingListViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace TastyFood.Core.Models.ShoppingListModels
+{
+    using TastyFood.Core.Models.IngredientModels;
+
+    public class CreateShoppingListViewModel
+    {
+        public CreateShoppingListViewModel()
+        {
+            this.Ingredients = new List<IngredientViewModel>();
+        }
+
+        public string Name { get; set; } = null!;
+
+        public string UserId { get; set; } = null!;
+
+        public string Username { get; set; } = null!;
+
+        public List<IngredientViewModel> Ingredients { get; set; } = null!;
+    }
+}
