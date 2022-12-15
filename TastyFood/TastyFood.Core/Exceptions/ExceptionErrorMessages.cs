845875871c22c0476b5d8ff81260e98f896489bd
Simeon Yordanov <lookmanche@gmail.com>
2022-12-15 13:17:53 +0200

    created class for exception messages

---
+++ TastyFood/TastyFood.Core/Exceptions/ExceptionErrorMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace TastyFood.Core.Exceptions
+{
+    public static class ExceptionErrorMessages
+    {
+        public const string AlreadyDeletedExceptionMessage = "The {0} with ID {1} does not exist";
+        public const string ArgumentNullExceptionMessage = "The {0} with ID {1} is already deleted";
+    }
+}
