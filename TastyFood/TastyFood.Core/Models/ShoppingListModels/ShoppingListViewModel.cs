bf2934af34a36d3c59fd6eb258ba9880fc1210d0
Simeon Yordanov <lookmanche@gmail.com>
2022-12-16 22:51:57 +0200

    created new View Model for MyShoppingLists page

---
+++ TastyFood/TastyFood.Core/Models/ShoppingListModels/ShoppingListViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TastyFood.Core.Models.ShoppingListModels
+{
+    using TastyFood.Core.Models.IngredientModels;
+
+    public class ShoppingListViewModel
+    {
+        public ShoppingListViewModel()
+        {
+            this.Ingredients = new List<IngredientViewModel>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<IngredientViewModel> Ingredients { get; set; }
+    }
+}
