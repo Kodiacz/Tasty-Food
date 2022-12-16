2885755cec697498d33b7087d6dcc2f736a8a48f
Simeon Yordanov <lookmanche@gmail.com>
2022-12-16 19:26:16 +0200

    new migration for the changes in Ingredient entity

---
+++ TastyFood/TastyFood.Infrastructure/Migrations/20221216172351_RecipeIdInIngredientsIsNull.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TastyFood.Infrastructure.Migrations
+{
+    public partial class RecipeIdInIngredientsIsNull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ingredients_Recipes_RecipeId",
+                table: "Ingredients");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "ShoppingLists",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RecipeId",
+                table: "Ingredients",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ingredients_Recipes_RecipeId",
+                table: "Ingredients",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ingredients_Recipes_RecipeId",
+                table: "Ingredients");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "ShoppingLists",
+                type: "bit",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RecipeId",
+                table: "Ingredients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ingredients_Recipes_RecipeId",
+                table: "Ingredients",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
